0c755bc9b7b42fd8c0fa9bc8e0b5bfb1a0eebc5f
edrianSandoval <ejsandoval@unimonserrate.edu.co>
2024-04-17 21:02:04 -0500

    prueba

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const loginRoutes = require('./routes/login');
 const viewsRoutes = require('./routes/views');
 
+prueba merge
 const app = express();
 
 app.set('port', 4000);

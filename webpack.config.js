6b9dfaf58c48a9ecbdd60afc7765084f56cd1ee1
Duvan Gonzalez <programmerloquillo@gmail.com>
2024-04-23 19:51:49 -0500

    Se ajusta layout main para todos los modulos

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './public/js/particles.js',
+  entry: [
+    './public/js/particles.js',
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'public/js'),
